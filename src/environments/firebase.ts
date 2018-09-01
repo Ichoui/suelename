2bd326f44560d3e88caa3f940b57185c30b08b50
Ichoui <nicol.morgan@outlook.fr>
2018-09-01 14:04:55 +0200

    Ajout firebaseTs

---
+++ src/environments/firebase.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  firebase: {
+    apiKey: 'AIzaSyBxT4-UDVTuOCnGF90djthURuaKXVq09KE',
+    authDomain: 'suelename-project.firebaseapp.com',
+    databaseURL: 'https://suelename-project.firebaseio.com',
+    projectId: 'suelename-project',
+    storageBucket: 'suelename-project.appspot.com',
+    messagingSenderId: '755769926571'
+  }
+};
